70d5e6f226d62d3dbbe00b5f7b66608c3dca1944
Gaurav <ojha.iiitm@gmail.com>
2020-05-09 02:24:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setup(
     ],
     install_requires=[
         'XBlock>=0.4.10,<2.0.0',
+        'xblock-utils>=1.2.0',
         'requests>=2.9.1,<3.0.0',
         'babelfish>=0.5.5,<0.6.0',
     ],
